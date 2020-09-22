94c13c7952b193e63df481efed6e004e7ec09526
Hoang Anh <anh.le@fujitechjsc.com>
2020-09-22 14:47:02 +0700

    List as card items

--- tab_page_camera/lib/src/screens/call_screen.dart
+++ tab_page_camera/lib/src/screens/call_screen.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 class CallsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text(
-        "Calls",
-        style: new TextStyle(fontSize: 20.0),
-      ),
-    );
+    return ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text('Oke'),
+            ),
+          );
+        });
   }
 }
