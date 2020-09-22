168e90c9da1b8237576ed1a2f3c482daa8bff2a6
Hoang Anh <anh.le@fujitechjsc.com>
2020-09-22 14:49:34 +0700

    update datasource

--- tab_page_camera/lib/src/screens/call_screen.dart
+++ tab_page_camera/lib/src/screens/call_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CallsScreen extends StatelessWidget {
+  var dataSource =
+      'Updates were rejected because the remote contains work that you do not have locally.'
+          .split(' ');
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 10,
+        itemCount: dataSource.length,
         itemBuilder: (context, index) {
           return Card(
             child: ListTile(
-              title: Text('Oke'),
+              title: Text(dataSource[index]),
             ),
           );
         });
